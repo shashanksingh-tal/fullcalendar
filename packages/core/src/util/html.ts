6846967dad9dd98474f5a13513ba848f0af4e697
shashanksingh-tal <shashank.singh@talentica.com>
2019-12-13 14:36:45 +0530

    Adding changes for calender

--- packages/core/src/util/html.ts
+++ packages/core/src/util/html.ts
@@ -1,11 +1,6 @@
 
 export function htmlEscape(s) {
-  return (s + '').replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/'/g, '&#039;')
-    .replace(/"/g, '&quot;')
-    .replace(/\n/g, '<br />')
+  return s;
 }
 
 
